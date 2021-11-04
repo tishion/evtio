5ad2124c0c397789b25ef9444d94f60eaac96d67
Sheen Tian Shen <sheentianshen@gmail.com>
2021-11-04 22:19:31 +0800

    first commit

---
+++ example/echo_tcp_server.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file echo_tcp_server.cpp
+ * @author Sheen Tian Shen (sheentianshen@gmail.com)
+ * @brief
+ * @date 2021-11-04
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "in main()" << std::endl;
+  return 0;
+}
---
+++ example/echo_udp_server.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file echo_udp_server.cpp
+ * @author Sheen Tian Shen (sheentianshen@gmail.com)
+ * @brief
+ * @date 2021-11-04
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <iostream>
+
+#include <evtio.hpp>
+
+int main(int argc, char* argv[]) {
+  std::cout << "in main()" << std::endl;
+  return 0;
+}
