093e4aad1812f3d99899abe7743cc622d37bc80d
Sheen Tian <5869938+tishion@users.noreply.github.com>
2021-11-09 19:31:41 +0800

    Update evtio.hpp

--- include/evtio.hpp
+++ include/evtio.hpp
@@ -340,7 +340,10 @@ protected:
     if (::kevent(kqfd_, &ev, 1, nullptr, 0, nullptr) < 0) {
       logE() << "failed to associate the wakeup event with kqueue:(" << errno << ")"
              << strerror(errno);
+      return false;
     }
+ 
+    return true;
   }
 
   void kqueue_remove_wakeup_event() {
